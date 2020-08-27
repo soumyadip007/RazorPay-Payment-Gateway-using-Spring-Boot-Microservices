3fec4942ca51216d699b480cb5cdd2b8dea757f8
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-08-27 23:30:28 +0530

    Razor Pay Initial Commit

---
+++ Razorpay/src/main/java/com/spring/gw/Razorpay/RazorpayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gw.Razorpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayApplication.class, args);
+	}
+
+}
---
+++ Razorpay/src/test/java/com/spring/gw/Razorpay/RazorpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gw.Razorpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
